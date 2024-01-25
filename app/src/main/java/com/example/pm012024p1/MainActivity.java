b9f59f8f79771ef46623dddc89e51b601fd39ad1
ricardo.lagos <ricardo.lagoshn@gmail.com>
2024-01-24 20:43:46 -0600

    Cambios realizados el 24/01/2024

--- app/src/main/java/com/example/pm012024p1/MainActivity.java
+++ app/src/main/java/com/example/pm012024p1/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.pm012024p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nombres, apellidos, telefono;
+    Button btnproceso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombres = (EditText) findViewById(R.id.nombres);
+        apellidos = (EditText) findViewById(R.id.apellidos);
+        telefono = (EditText) findViewById(R.id.telefono);
+        btnproceso = (Button) findViewById(R.id.btnprocesar);
+
+        btnproceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), nombres.getText().toString(), Toast.LENGTH_LONG ).show();
+            }
+        });
+
+
     }
 }
